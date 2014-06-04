7e58bfcd50ff4dff611fe6676620798f200f189e
java <wukenaihesos@gmail.com>
2014-06-04 17:19:59 +0800

    Generate Parentheses
    1、左右()用完了，入库
    2、坐括号添加肯定没问题
    3、如果，现在的位置左括号一定是要比右括号多的，要不然就非法了

---
+++ Leetcode/src/GenerateParentheses.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GenerateParentheses {
+	List<String> list=new ArrayList<String>();
+	public List<String> generateParenthesis(int n) {
+		gen(n, n, "");
+		return list;
+	}
+	
+	private void gen(int left,int right,String s){
+		if(left==0&&right==0){
+			list.add(s);
+		}
+		
+		if(left>0){
+			gen(left-1,right,s+"(");
+		}
+		
+		if(right>0&&left<right){
+			gen(left, right-1, s+")");
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		GenerateParentheses g=new GenerateParentheses();
+		List<String> list=g.generateParenthesis(4);
+		Collections.sort(list);
+		System.out.println(list);
+	}
+}
