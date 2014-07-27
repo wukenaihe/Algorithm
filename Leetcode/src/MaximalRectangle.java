52486c7c38363c9ed35c1212c11708942386b40f
Xumh <wukenaihesos@gmail.com>
2014-07-27 14:52:22 +0800

    Maximal Rectangle
    1、把长方形看成柱状体，求柱状体面积

--- Leetcode/src/MaximalRectangle.java
+++ Leetcode/src/MaximalRectangle.java
@@ -1,78 +1,45 @@
+import java.util.Stack;
+
 public class MaximalRectangle {
-	//正方形，错了
-//	public int maximalRectangle(char[][] matrix) {
-//		int l1=matrix.length;
-//		if(l1==0){
-//			return 0;
-//		}
-//		int l2=matrix[0].length;
-//		int max=0;
-//		for(int i=0;i<l1-max;++i){
-//			for(int j=0;j<l2-max;++j){
-//				char c=matrix[i][j];
-//				if(c=='1'){
-//					int k=0;
-//					while(i+k<l1&&j+k<l2){
-//						boolean rect=true;
-//						for(int z=i;z<=i+k;++z){
-//							char c1=matrix[z][j+k];
-//							if(c1!='1'){
-//								rect=false;
-//								break;
-//							}
-//						}
-//						for(int z=j;z<=j+k;++z){
-//							char c1=matrix[i+k][z];
-//							if(c1!='1'){
-//								rect=false;
-//								break;
-//							}
-//						}
-//						if(!rect){
-//							break;
-//						}
-//						if(k+1>max){
-//							max=k+1;
-//						}
-//						k++;
-//					}
-//				}
-//			}
-//		}
-//		return max;
-//		
-//	}
 	public int maximalRectangle(char[][] matrix){
 		int l1=matrix.length;
 		if(l1==0){
 			return 0;
 		}
+		int max=Integer.MIN_VALUE;
 		int l2=matrix[0].length;
-		int max=0;
 		for(int i=0;i<l1;++i){
-			int temp=0;
+			int[] height=new int[l2+1];
 			for(int j=0;j<l2;++j){
-				if(j==0&&matrix[i][j]=='1'){
-					temp=1;
-				}else if(matrix[i][j]=='1'&&matrix[i][j-1]=='1'){
-					temp++;
-					max=max<temp?temp:max;
-				}else if(matrix[i][j]=='1'&&matrix[i][j-1]!='1'){
-					temp=1;
+				int h=0;
+				for(int k=i;k>=0;--k){
+					if(matrix[k][j]=='1'){
+						h++;
+					}else{
+						height[j]=h;
+						break;
+					}
+					
+					if(k==0){
+						height[j]=h;
+					}
 				}
 			}
+			max=Math.max(max, maxArea(height));
 		}
-		for(int i=0;i<l2;++i){
-			int temp=0;
-			for(int j=0;j<l1;++j){
-				if(j==0&&matrix[j][i]=='1'){
-					temp=1;
-				}else if(matrix[j][i]=='1'&&matrix[j-1][i]=='1'){
-					temp++;
-				}else if(matrix[j][i]=='1'&&matrix[j-1][i]!='1'){
-					temp=1;	
-				}
-				max=max<temp?temp:max;
+		return max;
+	}
+	
+	private int maxArea(int[] height){
+		Stack<Integer> stack=new Stack<Integer>();
+		int max=0;
+		int i=0;
+		while(i<height.length){
+			if(stack.isEmpty()||height[i]>=height[stack.peek()]){
+				stack.push(i++);
+			}else{
+				int h=stack.pop();
+				max=Math.max(max, height[h]*(stack.isEmpty()?i:i-stack.peek()-1));
 			}
 		}
 		return max;
