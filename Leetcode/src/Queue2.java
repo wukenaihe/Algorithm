f9849a123541eab61cb1f03cd697152ce6ab31fc
Xumh <wukenaihesos@gmail.com>
2014-08-04 16:07:56 +0800

    N-Queue II

--- Leetcode/src/Queue2.java
+++ Leetcode/src/Queue2.java
@@ -1,55 +1,57 @@
+import java.util.Arrays;
+
 public class Queue2 {
-
-	int total = 0;
-
 	public int totalNQueens(int n) {
 		int[] nums = new int[n];
+		int count = 0;
 		for (int i = 0; i < n; ++i) {
-			nums[i] = i + 1;
+			nums[i] = i;
 		}
-		isOk(nums, 0, new int[n]);
-		return total;
+		while (true) {
+			if(ok(nums)){
+				count++;
+			}
+			int i = nums.length - 1;
+			while (i > 0 && nums[i - 1] > nums[i]) {
+				i--;
+			}
+			if(i<=0){
+				return count;
+			}
+			int q = i - 1;
+			while (i < nums.length && nums[i] > nums[q]) {
+				i++;
+			}
+			swap(nums, q, i-1);
+			q += 1;
+			i = nums.length - 1;
+			while (q < i) {
+				swap(nums, q++, i--);
+			}
+		}
 	}
 
-	private void isOk(int[] nums,int start,int[] pre){
-//		if(start==nums.length-1){
-//			pre[start]=nums[start];
-//			
-//			for(int i=0;i<start;++i){
-//				if(Math.abs(i-start)==Math.abs(pre[i]-nums[start])){
-//					break;
-//				}
-//			}
-//			
-//			total++;
-//			pre[start]=0;
-//			return;
-//		}
-//		for(int i=start;i<nums.length;++i){
-//			for(int j=0;j<start;++j){
-//				boolean ok=true;
-//				if(Math.abs(j-start)==Math.abs(pre[j]-nums[start])){
-//					ok=false;
-//					break;
-//				}
-//				if(!ok){
-//					break;
-//				}
-//			}
-//			int temp=nums[start];
-//			nums[start]=nums[i];
-//			nums[i]=temp;
-//			pre[start]=nums[start];
-//			isOk(nums, start+1, pre);
-//			temp=nums[start];
-//			nums[start]=nums[i];
-//			nums[i]=temp;
-//		}
+	private boolean ok(int[] nums) {
+		for(int i=0;i<nums.length-1;++i){
+			for(int j=i+1;j<nums.length;++j){
+				if((j-i)==Math.abs(nums[i]-nums[j])){
+					return false;
+				}
+			}
+		}
+		return true;
+
+	}
+
+	private void swap(int[] nums, int a, int b) {
+		int temp = nums[a];
+		nums[a] = nums[b];
+		nums[b] = temp;
 	}
 
 	public static void main(String[] args) {
 		Queue2 q = new Queue2();
-		System.out.println(q.totalNQueens(8));
+		System.out.println(q.totalNQueens(4));
 	}
 
 }
