697fbcc26c6d543220f6589587d994c77ddad130
Xumh <wukenaihesos@gmail.com>
2014-08-28 11:34:09 +0800

    Divide Two Integers

--- Leetcode/src/DivideTwoIntegers.java
+++ Leetcode/src/DivideTwoIntegers.java
@@ -2,36 +2,37 @@
 public class DivideTwoIntegers {
 	public int divide(int dividend, int divisor) {
 		if(divisor==0){
-			return Integer.MAX_VALUE;
+		    return Integer.MAX_VALUE;
 		}
 		int res=0;
 		if(dividend==Integer.MIN_VALUE){
-			res=1;
-			dividend+=Math.abs(divisor);
+		    res=1;
+		    dividend+=Math.abs(divisor);
 		}
 		if(divisor==Integer.MIN_VALUE){
-			return res;
+		    return res;
 		}
 		boolean isNeg=((dividend^divisor)>>>31)==1?true:false;
 		
-		int digit=0;
 		dividend=Math.abs(dividend);
 		divisor=Math.abs(divisor);
 		
+		int count=0;
 		while(divisor<=(dividend>>1)){
-			digit++;
-			divisor<<=1;
+		    count++;
+		    divisor<<=1;
 		}
 		
-		while(digit>=0){
-			if(dividend>=divisor){
-				dividend-=divisor;
-				res+=1<<digit;
-			}
-			digit--;
-			divisor>>=1;
+		while(count>0){
+		    if(dividend>=divisor){
+		        dividend-=divisor;
+		        res+=1<<count;
+		    }
+		    count--;
+		    divisor>>=1;
 		}
-        return isNeg?-res:res;
+		
+		return isNeg?-res:res;
     }
 	
 	public static void main(String[] args) {
