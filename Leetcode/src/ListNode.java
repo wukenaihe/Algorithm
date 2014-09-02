3f31b8f28e830420fcd19a2c0942e478e637077d
Xumh <wukenaihesos@gmail.com>
2014-09-02 20:11:26 +0800

    Merge K list
    采用堆的方式归并

--- Leetcode/src/ListNode.java
+++ Leetcode/src/ListNode.java
@@ -6,4 +6,15 @@ public class ListNode {
 		val = x;
 		next = null;
 	}
+	
+	public static void print(ListNode list){
+		while(list!=null){
+			System.out.print(list.val+"-");
+			list=list.next;
+		}
+	}
+	
+	public String toString(){
+		return String.valueOf(val);
+	}
 }
--- Leetcode/src/MergeKLists.java
+++ Leetcode/src/MergeKLists.java
@@ -1,86 +1,43 @@
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class MergeKLists {
-//	public ListNode mergeKLists(List<ListNode> lists) {
-//		ListNode l=null;
-//		ListNode h=null;
-//		while(lists.size()!=0){
-//			ListNode p=null;
-//			int min=Integer.MAX_VALUE;
-//			int minIndex=0;
-//			for (int i=0;i<lists.size();++i) {
-//				ListNode listNode=lists.get(i);
-//				if(listNode==null){
-//					minIndex=i;
-//					break;
-//				}
-//				if(listNode.val<min){
-//					p=listNode;
-//					min=listNode.val;
-//					minIndex=i;
-//				}
-//			}
-//			if(p==null){
-//				lists.remove(minIndex);
-//				continue;
-//			}
-//			if(l==null){
-//				l=p;
-//				h=p;
-//			}else{
-//				l.next=p;
-//				l=p;
-//			}
-//			if (p.next==null) {
-//				lists.remove(minIndex);
-//			}else{
-//				lists.set(minIndex, p.next);
-//			}
-//			
-//			
-//		}
-//		return h;
-//	}
+
 	
 	public ListNode mergeKLists(List<ListNode> lists) {
 		if(lists.size()==0){
 			return null;
 		}
-		ListNode l=mergeKLists(lists, 0, lists.size()-1);
-		return l;
-	}
-	
-	public ListNode mergeKLists(List<ListNode> lists,int start,int end){
-		if(start==end){
-			return lists.get(start);
-		}
-		int mid=(start+end)/2;
-		ListNode l1=mergeKLists(lists, start,mid);
-		ListNode l2=mergeKLists(lists, mid+1, end);
+		ListNode head=new ListNode(0);
+		ListNode cur=head;
 		
-		ListNode h=new ListNode(0);
-		ListNode p=h;
-		while(l1!=null&&l2!=null){
-			if(l1.val<l2.val){
-				p.next=l1;
-				l1=l1.next;
-			}else{
-				p.next=l2;
-				l2=l2.next;
+		PriorityQueue<ListNode> queue=new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>() {
+
+			@Override
+			public int compare(ListNode o1, ListNode o2) {
+				return o1.val-o2.val;
 			}
-			p=p.next;
-		}
+			
+		});
 		
-		if(l1!=null){
-			p.next=l1;
+		for (ListNode listNode : lists) {
+			if(listNode!=null){
+				queue.add(listNode);
+			}
+		}		
+		while(!queue.isEmpty()){
+			ListNode l=queue.poll();
+			if(l.next!=null){
+				queue.add(l.next);
+			}
+			cur.next=l;
+			cur=l;
 		}
+		cur.next=null;
 		
-		if(l2!=null){
-			p.next=l2;
-		}
-		
-		return h.next;
+		return head.next;
 	}
 	
 	public static void main(String[] args) {
@@ -99,8 +56,6 @@ public class MergeKLists {
 		list.add(l3);
 		
 		ListNode l=m.mergeKLists(list);
-		System.out.println(l);
-		
-		
+		ListNode.print(l);
 	}
 }
