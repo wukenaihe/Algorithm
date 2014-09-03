d8371d973ad0e4decf14accbc1e74ef903312fcb
Xumh <wukenaihesos@gmail.com>
2014-09-03 19:09:08 +0800

    Remove Nth Node From End of List

--- Leetcode/src/RemoveNthNodeFromEndList.java
+++ Leetcode/src/RemoveNthNodeFromEndList.java
@@ -2,23 +2,25 @@
 public class RemoveNthNodeFromEndList {
 	
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-		ListNode pre=head;
-		ListNode p=head;
-		int count=0;
-		for(;count<n;++count){
-			pre=pre.next;
-		}
-		if(pre==null){
-			return head.next;
-		}
-		while(pre.next!=null){
-			pre=pre.next;
-			p=p.next;
-		}
-		p.next=p.next.next;
-		
-		return head;
-	}
+        if(head==null){
+            return null;
+        }
+        ListNode pre=head,end=head;
+        for(int i=0;i<n&&end!=null;++i){
+            end=end.next;
+        }
+        if(end==null){
+            return head.next;
+        }
+        while(end.next!=null){
+            end=end.next;
+            pre=pre.next;
+        }
+        
+        pre.next=pre.next.next;
+        return head;
+        
+    }
 	public static void main(String[] args) {
 		ListNode l1=new ListNode(1);
 		ListNode l2=new ListNode(2);
@@ -26,14 +28,14 @@ public class RemoveNthNodeFromEndList {
 		ListNode l4=new ListNode(4);
 		ListNode l5=new ListNode(5);
 		
-//		l1.next=l2;
-//		l2.next=l3;
-//		l3.next=l4;
-//		l4.next=l5;
+		l1.next=l2;
+		l2.next=l3;
+		l3.next=l4;
+		l4.next=l5;
 		
 		RemoveNthNodeFromEndList r=new RemoveNthNodeFromEndList();
-		ListNode l=r.removeNthFromEnd(l1, 1);
-		System.out.println(l);
+		ListNode l=r.removeNthFromEnd(l1, 2);
+		ListNode.print(l);
 		
 		
 	}
