617d945d85ea5aa9031a9b09d865e34b210c4ac3
Xumh <wukenaihesos@gmail.com>
2014-09-06 14:47:49 +0800

    Longest Substring Without Repeating Characters

--- Leetcode/src/LongestSubstringWithoutRepeatingCharacters.java
+++ Leetcode/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,27 +1,24 @@
 public class LongestSubstringWithoutRepeatingCharacters {
 	public int lengthOfLongestSubstring(String s) {
-		int[] hash = new int[256];
-		int max = 0;
-		int temp = 0;
-		int start = 0;
-		int end = s.length();
-		while (start < end) {
-			char c = s.charAt(start);
-			if (hash[c] != 0) {
-				max = max < temp ? temp : max;
-				start = hash[c];
-				hash = new int[256];
-				temp=0;
-			} else {
-				hash[c] = start+1;
-				temp++;
-				max = max < temp ? temp : max;
-				start++;
-			}
-		}
-
-		return max;
-	}
+        boolean[] tag=new boolean[256];
+        int max=0,count=0;
+        int start=0,end=0;
+        while(end<s.length()){
+            while(end<s.length()&&tag[s.charAt(end)]==false){
+                count++;
+                tag[s.charAt(end++)]=true;
+            }
+            max=max<count?count:max;
+            while(end<s.length()&&s.charAt(end)!=s.charAt(start)){
+                tag[s.charAt(start)]=false;
+                start++;
+                count--;
+            }
+            start++;
+            end++;
+        }
+        return max;
+    }
 
 	public static void main(String[] args) {
 		LongestSubstringWithoutRepeatingCharacters l = new LongestSubstringWithoutRepeatingCharacters();
@@ -30,5 +27,8 @@ public class LongestSubstringWithoutRepeatingCharacters {
 		System.out
 				.println(l
 						.lengthOfLongestSubstring("hnwnkuewhsqmgbbuqcljjivswmdkqtbxixmvtrrbljptnsnfwzqfjmafadrrwsofsbcnuvqhffbsaqxwpqcac"));
+		System.out.println(l.lengthOfLongestSubstring("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco"));
+		System.out.println(l.lengthOfLongestSubstring("bbb"));
+		System.out.println(l.lengthOfLongestSubstring("whtaciohordtqkvwcsgspqo"));
 	}
 }
