568ab7e379ab271d74036a5cc41bf5cfda8fe6fd
Xumh <wukenaihesos@gmail.com>
2014-09-11 09:59:43 +0800

    Reorder List

--- Leetcode/src/ReorderList.java
+++ Leetcode/src/ReorderList.java
@@ -1,72 +1,45 @@
 
 public class ReorderList {
-	static class ListNode {
-		int val;
-		ListNode next;
-
-		ListNode(int x) {
-			val = x;
-			next = null;
-		}
-	}
-
 	public void reorderList(ListNode head) {
-		if (head == null || head.next == null || head.next.next == null) {
-			return;
-		}
-		ListNode p1, p2, p3 = null, p4 = null;
-		p1 = head;
-		p2 = head;
-		while (p2 != null && p2.next != null) {
-			p3 = p1;
-			p1 = p1.next;
-			p2 = p2.next.next;
-		}
-
-		if (p2 == null) {
-			p3.next = null;
-			p2 = p1;
-			p1 = head;
-		} else {
-			p2 = p1.next;
-			p1.next = null;
-			p1 = head;
-		}
-		p2=reverse(p2);
-		
-		while (p2 != null) {
-			p3 = p1.next;
-			p1.next = p2;
-			p4 = p2.next;
-			p2.next = p3;
-			p2 = p4;
-			p1 = p3;
-		}
-	}
-	
-	public ListNode reverse(ListNode p){
-		if(p==null){
-			return p;
-		}
-		ListNode p1=p,p2=p.next,p3;
-		p1.next=null;
-		while(p2!=null){
-			p3=p2.next;
-			p2.next=p1;
-			p1=p2;
-			p2=p3;
-		}
-		
-		return p1;
-	}
-
-	private void print(ListNode l) {
-		while (l != null) {
-			System.out.print(l.val + " ");
-			l = l.next;
-		}
-
-	}
+        if(head==null){
+            return;
+        }
+        ListNode fast=head,slow=head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode nextHead=slow.next;
+        slow.next=null;
+        
+        nextHead=reverse(nextHead);
+        ListNode p=head;
+        slow=head;
+        fast=nextHead;
+        while(slow!=null&&fast!=null){
+            ListNode s=slow.next;
+            slow.next=fast;
+            ListNode f=fast.next;
+            fast.next=s;
+            slow=s;
+            fast=f;
+        }
+        
+    }
+    
+    private ListNode reverse(ListNode head){
+        if(head==null){
+            return null;
+        }
+        ListNode pre=null,p=head;
+        while(p!=null){
+            ListNode temp=p.next;
+            p.next=pre;
+            pre=p;
+            p=temp;
+        }
+        return pre;
+    }
 
 	public static void main(String[] args) {
 		ListNode l1 = new ListNode(1);
@@ -83,7 +56,7 @@ public class ReorderList {
 		ReorderList re = new ReorderList();
 		re.reorderList(l1);
 
-		re.print(l1);
+		ListNode.print(l1);
 
 	}
 }
