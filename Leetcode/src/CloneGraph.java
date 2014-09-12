14735f3c303748d20a76a55a5769b0a88f70d6a7
Xumh <wukenaihesos@gmail.com>
2014-09-12 09:51:37 +0800

    Clone Graph
    添加一个广度搜索的

--- Leetcode/src/CloneGraph.java
+++ Leetcode/src/CloneGraph.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class CloneGraph {
 
@@ -14,28 +17,55 @@ public class CloneGraph {
 		}
 	}
 	
-	Map<Integer, UndirectedGraphNode> map=new HashMap<Integer, CloneGraph.UndirectedGraphNode>();
-
+//	Map<Integer, UndirectedGraphNode> map=new HashMap<Integer, CloneGraph.UndirectedGraphNode>();
+//
+//	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+//		if(node==null){
+//			return null;
+//		}
+//		int label=node.label;
+//		UndirectedGraphNode c=map.get(label);
+//		if(c!=null){
+//			return c;
+//		}else{
+//			c=new UndirectedGraphNode(label);
+//			c.neighbors=new ArrayList<UndirectedGraphNode>();
+//			map.put(label, c);
+//			ArrayList<UndirectedGraphNode> nodes=node.neighbors;
+//			for (UndirectedGraphNode undirectedGraphNode : nodes) {
+//				UndirectedGraphNode clone=cloneGraph(undirectedGraphNode);
+//				c.neighbors.add(clone);
+//			}
+//		}
+//		return c;
+//	}
+	
 	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-		if(node==null){
-			return null;
-		}
-		int label=node.label;
-		UndirectedGraphNode c=map.get(label);
-		if(c!=null){
-			return c;
-		}else{
-			c=new UndirectedGraphNode(label);
-			c.neighbors=new ArrayList<UndirectedGraphNode>();
-			map.put(label, c);
-			ArrayList<UndirectedGraphNode> nodes=node.neighbors;
-			for (UndirectedGraphNode undirectedGraphNode : nodes) {
-				UndirectedGraphNode clone=cloneGraph(undirectedGraphNode);
-				c.neighbors.add(clone);
+        if(node==null){
+            return null;
+        }
+        Map<UndirectedGraphNode, UndirectedGraphNode> map=new HashMap<CloneGraph.UndirectedGraphNode, CloneGraph.UndirectedGraphNode>();
+        Queue<UndirectedGraphNode> queue=new LinkedList<>();
+        queue.add(node);
+        UndirectedGraphNode copyGraph=new UndirectedGraphNode(node.label);
+        map.put(node, copyGraph);
+        while(!queue.isEmpty()){
+        	UndirectedGraphNode n=queue.poll();
+        	UndirectedGraphNode c=map.get(n);
+        	ArrayList<UndirectedGraphNode> neighbors=n.neighbors;
+        	for (UndirectedGraphNode undirectedGraphNode : neighbors) {
+				if(!map.containsKey(undirectedGraphNode)){
+					queue.add(undirectedGraphNode);
+					UndirectedGraphNode copy=new UndirectedGraphNode(undirectedGraphNode.label);
+					map.put(undirectedGraphNode,copy);
+					c.neighbors.add(copy);
+				}else{
+					c.neighbors.add(map.get(undirectedGraphNode));
+				}
 			}
-		}
-		return c;
-	}
+        }
+        return copyGraph;
+    }
 	
 	public static void main(String[] args) {
 		UndirectedGraphNode head=new UndirectedGraphNode(0);
