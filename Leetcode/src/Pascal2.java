00186b353a9a2568bf1430382b228ff83429029c
Xumh <wukenaihesos@gmail.com>
2014-09-14 09:54:15 +0800

    Pascal's Triangle II

--- Leetcode/src/Pascal2.java
+++ Leetcode/src/Pascal2.java
@@ -1,30 +1,36 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Pascal2 {
-	public ArrayList<Integer> getRow(int rowIndex) {
-		ArrayList<Integer> list=new ArrayList<Integer>(rowIndex);
-		list.add(1);
-		if(rowIndex==0) return list;
-		list.add(1);
-		if(rowIndex==1) return list;
-		
-		for(int i=2;i<=rowIndex;++i){
-			int temp=list.get(0)+list.get(1);
-			for(int j=2;j<i;++j){
-				int num=list.get(j-1)+list.get(j);
-				list.set(j-1, temp);
-				temp=num;
-			}
-			list.set(i-1, temp);
-			list.add(1);
-		}
-		
-		
-		return list;
-	}
+	public List<Integer> getRow(int rowIndex) {
+        List<Integer> list=new ArrayList<Integer>();
+        if(rowIndex==0){
+        	list.add(1);
+            return list;
+        }
+        Integer[] num=new Integer[rowIndex+1];
+        num[0]=1;
+        for(int i=1;i<=rowIndex;++i){
+            int temp=num[0];
+            for(int j=1;j<=i;++j){
+                int res=0;
+                if(j!=i){
+                    res=temp+num[j];
+                    temp=num[j];
+                }else{
+                    res=1;
+                    temp=1;
+                }
+                num[j]=res;
+            }
+        }
+        list.addAll(Arrays.asList(num));
+        return list;
+    }
 	
 	public static void main(String[] args) {
 		Pascal2 p=new Pascal2();
-		System.out.println(p.getRow(0));
+		System.out.println(p.getRow(5));
 	}
 }
