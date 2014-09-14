63f76a4fb96e723f8a9d1e94df37d0353a926ef8
Xumh <wukenaihesos@gmail.com>
2014-09-14 09:36:12 +0800

    Triangle

--- Leetcode/src/Triangle.java
+++ Leetcode/src/Triangle.java
@@ -1,47 +1,41 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Triangle {
-	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-		int length=triangle.size();
-		int[] sum1=new int[length];
-		int[] sum2=new int[length];
-		sum1[0]=triangle.get(0).get(0);
-		for(int i=1;i<length;++i){
-			ArrayList<Integer> list=triangle.get(i);
-			int l=list.size();
-			sum2[0]=sum1[0]+list.get(0);
-			for(int j=1;j<l-1;++j){
-				sum2[j]=Math.min(sum1[j-1]+list.get(j), sum1[j]+list.get(j));
-			}
-			sum2[l-1]=sum1[l-2]+list.get(l-1);
-			sum1=Arrays.copyOf(sum2, length);
-		}
-		
-		int min=Integer.MAX_VALUE;
-		for(int i=0;i<length;++i){
-			if(min>sum1[i]){
-				min=sum1[i];
-			}
-		}
-		
-		return min;
-	}
+	public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle==null){
+            return 0;
+        }
+        List<Integer> bottom=triangle.get(triangle.size()-1);
+        int[] res=new int[bottom.size()];
+        for(int i=0;i<bottom.size();++i){
+            res[i]=bottom.get(i);
+        }
+        
+        for(int i=triangle.size()-2;i>=0;--i){
+            List<Integer> list=triangle.get(i);
+            for(int j=0;j<list.size();++j){
+                res[j]=Math.min(res[j],res[j+1])+list.get(j);
+            }
+        }
+        return res[0];
+    }
 	
 	public static void main(String[] args) {
 		Triangle t=new Triangle();
-		ArrayList<ArrayList<Integer>> triangle=new ArrayList<ArrayList<Integer>>();
+		ArrayList<List<Integer>> triangle=new ArrayList<List<Integer>>();
 		
-		ArrayList<Integer> l1=new ArrayList<Integer>();
+		List<Integer> l1=new ArrayList<Integer>();
 		l1.add(2);
 		triangle.add(l1);
 		
-		ArrayList<Integer> l2=new ArrayList<Integer>();
+		List<Integer> l2=new ArrayList<Integer>();
 		l2.add(3);
 		l2.add(4);
 		triangle.add(l2);
 		
-		ArrayList<Integer> l3=new ArrayList<Integer>();
+		List<Integer> l3=new ArrayList<Integer>();
 		l3.add(6);
 		l3.add(5);
 		l3.add(7);
